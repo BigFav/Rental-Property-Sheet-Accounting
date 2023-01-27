a7b97b27fb0018007b7331b9653c07881a043eeb
BigFav <fnc4@cornell.edu>
2023-01-27 00:26:07 -0500

    Add a basic main with some documentation on next steps.

---
+++ main.py
@@ -0,0 +1,10 @@
+
+# Looking to have rows with the following columns:
+# Date,Category,Address,Apt#,Amount,Description,SourcedFrom,TaxDemarcation
+
+# The input reader should convert the input into a "request" object, with
+# appropriate defaults. From there various helpers can simply operate on that
+# object.
+
+def add_transaction():
+    pass
